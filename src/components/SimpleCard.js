81f9d9eef5e6a90a80abdd6356dcb6efa91801b1
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-09-20 06:24:17 -0700

    adjust height

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -7,7 +7,7 @@ const SimpleCard = ({movie}) => {
     const {image, name} = movie;
   return (
         <Card className="m-5 p-5">
-            <CardImg src={image} alt={name} width='100%' height='2600px'/>
+            <CardImg src={image} alt={name} width='100%'/>
             <CardImgOverlay>
                 <CardTitle style={{color:"ButtonFace", fontFamily:"cursive", fontSize:"10rem", backgroundColor:"ActiveCaption",}}>{name}</CardTitle>
             </CardImgOverlay>
