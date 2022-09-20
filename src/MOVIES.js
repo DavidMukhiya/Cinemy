6005a681014fdeb1c0fea9dd518fe7b3eae31cd7
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-09-19 22:47:46 -0700

    Added Movies.js as a source for DB for frontend

---
+++ src/MOVIES.js
@@ -0,0 +1,46 @@
+import avenger from './img/movies/avenger.jpg'
+import batman from './img/movies/batman.webp'
+import godfather from './img/movies/godfather.jpg'
+import jurassicpark from './img/movies/jurassic-park.jpg'
+import spiderman from './img/movies/spiderman.jpg'
+import titanic from './img/movies/avenger.jpg'
+
+
+export const MOVIES = [
+    {
+        id: 0,
+        name: 'Avenger',
+        image: avenger,
+        description: 'Amet incididunt incididunt pariatur non eiusmod commodo non. Ex reprehenderit ut laboris proident commodo aute amet ex est ullamco enim aliqua. Irure ullamco eu elit labore officia nostrud eiusmod dolor.'
+    },
+    {
+        id: 1,
+        name: 'Batman',
+        image: batman,
+        description: 'Commodo aute eu commodo laboris sint. Laboris eiusmod voluptate nisi elit. Dolore amet non enim eiusmod labore voluptate nulla voluptate dolore officia aliqua deserunt. Commodo tempor incididunt labore non nulla.'
+    },
+    {
+        id: 2,
+        name: 'Godfather',
+        image: godfather,
+        description: 'Amet incididunt incididunt pariatur non eiusmod commodo non. Ex reprehenderit ut laboris proident commodo aute amet ex est ullamco enim aliqua. Irure ullamco eu elit labore officia nostrud eiusmod dolor.'
+    },
+    {
+        id: 3,
+        name: 'Jurassic Park',
+        image: jurassicpark,
+        description: 'Amet incididunt incididunt pariatur non eiusmod commodo non. Ex reprehenderit ut laboris proident commodo aute amet ex est ullamco enim aliqua. Irure ullamco eu elit labore officia nostrud eiusmod dolor.'
+    },
+    {
+        id: 4,
+        name: 'Spiderman',
+        image: spiderman,
+        description: 'Amet incididunt incididunt pariatur non eiusmod commodo non. Ex reprehenderit ut laboris proident commodo aute amet ex est ullamco enim aliqua. Irure ullamco eu elit labore officia nostrud eiusmod dolor.'
+    },
+    {
+        id: 5,
+        name: 'Titanic',
+        image: titanic,
+        description: 'Amet incididunt incididunt pariatur non eiusmod commodo non. Ex reprehenderit ut laboris proident commodo aute amet ex est ullamco enim aliqua. Irure ullamco eu elit labore officia nostrud eiusmod dolor.'
+    },
+]
