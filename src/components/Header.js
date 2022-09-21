51c1a929518b514878f0bd52bc1eaedb859d1819
David Prakash Mukhiya <sherlock.41v94@gmail.com>
2022-09-20 23:38:28 -0700

    updated navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,38 @@
 import React from "react";
-import { Navbar } from "./Navbar";
+//import { Navbar } from "./Navbar";
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem  } from "reactstrap";
+import { NavLink } from "react-router-dom";
+import cinemylogo from '../img/cinemy-logo.jpg'
 
 const Header = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Navbar dark sticky="top" color="dark" expand="md" >
+      <NavbarBrand href="/" >
+        <img src={cinemylogo} alt="cinemy logo" width='70' />
+      </NavbarBrand>
+      <Nav className="me-auto" navbar>
+        <NavItem>
+            <NavLink className='nav-link' to='/'>
+              Home
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/movies'>
+              Movie
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/tvseries'>
+              TvSeries
+            </NavLink>
+        </NavItem>
+        <NavItem>
+            <NavLink className='nav-link' to='/admin'>
+              admin
+            </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
   );
 };
 
